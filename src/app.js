9ad931fa231154370dde98aeb480180ec35888c5
jonatasjdb <jonatasjdb51@gmail.com>
2020-04-08 21:26:21 -0300

    Desafio 1 Node.js Concluído.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,85 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = [{ id: uuid(), title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const id = uuid();
+  let likes = 0;
+  const repo = {id,title,url,techs,likes}
+  repositories.push(repo)
+  response.json({id});
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title,url,techs} = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  
+  if(repoIndex < 0)
+  {
+    return response.status(400).json({"error": "ID not found."});
+  }
+  const {likes} = repositories[repoIndex]
+  
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+  console.log(repositories[repoIndex])
+  repositories[repoIndex] = repo;
+
+  response.json({repo})
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  
+  if(repoIndex < 0)
+  {
+    return res.status(400).json({"error": "ID not found."});
+  }
+
+  repositories.splice(repoIndex,1);
+
+  return res.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+
+  if(repoIndex < 0){
+    return response.status(404).json({"error": "ID not found."});
+  }
+  let {likes} = repositories[repoIndex]
+  likes += 1
+  
+  repositories[repoIndex].likes = likes;
+
+  response.json(repositories[repoIndex])
+ 
+
 });
 
 module.exports = app;
